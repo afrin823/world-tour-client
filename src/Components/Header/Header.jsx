1ef966ed21a8d969388d0ab09a37899065bccd47
afrin823 <afrin12490@gmail.com>
2025-01-06 21:07:25 +0600

    haeader menu change

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -82,7 +82,9 @@ const Header = () => {
           {links}
         </ul>
       </div>
+      {/* navbar end  */}
       <div className="navbar-end">
+
         {
           user ? <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
@@ -103,14 +105,6 @@ const Header = () => {
             :
             <Link to={'/login'}><button className="btn btn-warning text-white">LogIn</button></Link>
         };
-        {/* <label className="flex cursor-pointer gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="5" /><path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" /></svg>
-          <input 
-          onChange={handleToggle}
-          type="checkbox"
-            className="toggle theme-controller" />
-          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>
-        </label> */}
       </div>
     </div>
   );
